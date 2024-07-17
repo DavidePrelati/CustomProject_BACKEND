e488348f49ba3be58a50839c5f9d81c806a9cae7
DavidePrelati <davide.prelati@gmail.com>
2024-07-17 12:55:21 +0200

    create_project

---
+++ custom_project_BACKEND/src/main/java/davide/prelati/custom_project_BACKEND/CustomProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package davide.prelati.custom_project_BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomProjectBackendApplication.class, args);
+	}
+
+}
---
+++ custom_project_BACKEND/src/test/java/davide/prelati/custom_project_BACKEND/CustomProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package davide.prelati.custom_project_BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
